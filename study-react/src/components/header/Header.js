84b35d932b72b96cd803bac7cb6610d78115cfed
woaks143@naver.com <woaks143@naver.com>
2023-04-12 18:38:26 +0900

    1. 카카오 로그인 구현

--- study-react/src/components/header/Header.js
+++ study-react/src/components/header/Header.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, {  useEffect, useState, useRef } from 'react';
 import styled from "styled-components";
 import {Button, Nav} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {ModalPopup} from '../../components/modal/ModalPopup'
-
+import * as common from '../../common/CommonFunction'
+import kakao from '../../img/kakao_login.png'
 
 const HeaderDiv = styled.div`
     position: relative;
@@ -32,17 +32,119 @@ const HeaderA = styled(Nav.Link)`
     margin-left : ${props=> (props.index) ? '70px' : ''};
 `
 
+const LoginSpan = styled.span`
+    font-size: 16px;
+    color: #fff;
+    margin-right: 40px;
+    display : none;
+`
+const ModalBack = styled.div`
+width: 100%;
+height: 100%;
+position: fixed;
+overflow: hidden;
+background: rgb(169 169 169 / 84%);
+z-index: 10;
+`
+
+const ModalPop = styled.div`
+    width: 400px;
+    height: 100px;
+    position: absolute;
+    top: 50%;
+    right: 40%;
+    z-index: 10;
+    background: #fff;
+    border : 1px solid;
+    border-radius : 10px;
+    animation: modal-show 0.3s;
+`
+
+const ModalButton = styled.button`
+float: right;
+background: none;
+border: none;
+font-weight: bold;
+font-size: 30px;
+position: relative;
+top: -13px;
+`
+
+const ButtonDiv = styled.div`
+display: grid;
+justify-content: center;
+top: 30%;
+position: relative;
+left: 20px;
+`
+
+const DivImg = styled.img`
+    cursor : pointer;
+`
+
+const DivImg2 = styled.img`
+cursor : pointer;
+position: relative;
+margin-top : 5px;
+width: 185px;
+height: 43px;
+border-radius: 6px;
+`
+
+const IoginImage = styled.img`
+    width: 35px;
+    border-radius: 15px;
+    margin-right: 5px;
+    display : none;
+`
 
 
-const Header = () => {
+const CLIENT_ID = "a2c2f50330b7526ae055d9a42ca1ac8e";
+const REDIRECT_URI =  "http://localhost:3000/KakaoCallback";
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+
+
+
+
+export const Header = () => {
+    const buttonRef = useRef(null);
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    
+    const [spanValue, setSpanValue] = useState(null);
+    const [loginImg, setLoginImg] = useState(null);
+    const [spanStyle, setSpanStyle] = useState('none');
 
-    const FnClickLogin = (flag) =>{ 
-        flag ? setModalIsOpen(true) :  setModalIsOpen(false);
+  
+    useEffect(()=>{
+        if(localStorage.getItem('token')){
+            setSpanValue(localStorage.getItem('name'));
+            setSpanStyle('inline');
+            setLoginImg(localStorage.getItem('img'));
+            buttonRef.current.innerText = '로그아웃';
+        }
+    },[])
+
+    window.addEventListener('message', function(event) {
+        if(event.data.style){
+            setSpanValue(event.data.name);
+            setSpanStyle(event.data.style);
+            setLoginImg(event.data.img)
+            setModalIsOpen(false);
+            buttonRef.current.innerText = '로그아웃';
+        }
+     });
+
+    const FnClickLogin = (flag,) =>{ 
+        if(buttonRef.current.innerText === '로그인'){
+            flag ? setModalIsOpen(true) :  setModalIsOpen(false);
+        }else{
+            if(window.confirm("로그아웃 하시겠습니까?")){
+                common.kakaoLogout();
+            }
+            
+        }
+        
     }
-
-
 
     return( 
         <>
@@ -53,16 +155,27 @@ const Header = () => {
                 <Nav className='me-auto'>
                     <HeaderA>커뮤니티</HeaderA>
                     <HeaderA index='2'>지도</HeaderA>
-
                 </Nav>
             </HeaderDiv>
             <HeaderDiv>
-                {/* <HeaderInput type="string" placeholder='아이디'/>
-                <HeaderInput input='true' type='passwd' placeholder='패스워드'/> */}
-                <HeaderButton onClick={()=>FnClickLogin(true)}>로그인</HeaderButton>
-                {/* <HeaderButton>회원가입</HeaderButton> */}
+                <IoginImage src={loginImg} style={{display:spanStyle}} alt='로그인'></IoginImage>
+                <LoginSpan style={{display:spanStyle}}><strong>{spanValue}</strong> 님 환영합니다.</LoginSpan>
+                <HeaderButton ref={buttonRef} onClick={()=>FnClickLogin(true)}>로그인</HeaderButton>
+
             </HeaderDiv>
-            { modalIsOpen && <ModalPopup close= {()=>FnClickLogin(false)}/> }
+            { modalIsOpen && 
+                <>
+                    <ModalBack>
+                        <ModalPop>
+                                <ModalButton onClick={()=>FnClickLogin(false)}>x</ModalButton>
+                                <ButtonDiv>
+                                        <DivImg src={kakao} onClick={()=>common.WindowPopup({KAKAO_AUTH_URL}, 'login')}></DivImg>
+                                        {/* <DivImg2 src={Github}></DivImg2> */}
+                                </ButtonDiv>
+                            </ModalPop>
+                    </ModalBack>
+                </>
+            }
         </>
     );
 }
@@ -70,4 +183,3 @@ const Header = () => {
 
 
 
-export default Header;
--- study-react/src/components/login/KakaoCallback.js
+++ study-react/src/components/login/KakaoCallback.js
@@ -1,16 +1,50 @@
-import react , {useEffect}from 'react'
-import Routh from 'react-router-dom'
+import react , {useEffect, useState, useContext}from 'react'
+import {CLIENT_ID, REDIRECT_URI} from '../modal/ModalPopup'
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
-export const Kakaologin = (props) => {
-    console.log('aaa');
-    useEffect(()=>{
+export const Kakaologin = () => {
+    const dispatch = useDispatch(); // dispatch로 재선언하여 사용한다.
+    const select = useSelector((state) => state.counter);
+    console.log(select);
+    
+    const code = new URL(window.location.href).searchParams.get('code');
+    const tokenUrl = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${CLIENT_ID}&redirect_url=${REDIRECT_URI}&code=${code}`
+
+    axios.post(tokenUrl,{},{
+        headers : {
+            "Content-Type" : "application/json;charset=UTF-8"
+        }
+    }
+    )
+    .then((res) => {
+        if(res.data){
+            localStorage.setItem('token', res.data.access_token);
+            let access_token = res.data.access_token;
+            axios.post(`https://kapi.kakao.com/v2/user/me`,{},{
+                headers : {
+                    Authorization: `Bearer ${access_token}`
+                }
+            })
+            .then((res)=>{
+                window.opener.postMessage({style:'inline', name: res.data.properties.nickname, img : res.data.properties.profile_image});
+                localStorage.setItem('name', res.data.properties.nickname);
+                localStorage.setItem('img', res.data.properties.profile_image);
+                window.close();
+            });
+        }else{
+            window.close();
+        }
         
-        const params = new URL(document.location.toString()).searchParams;
-        console.log(params);
-    },[])
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+
 
     return(
         <></>
     )
 
 }
+
